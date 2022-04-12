3d24b804f2ec43f1c299e83384915087e38951b4
Ingris Andrade <iac22@outlook.com>
2022-04-12 15:31:23 -0300

    Implementa a função average que recebe um array (tamanho variável) e retorna a média dos valores recebidos

--- src/average.js
+++ src/average.js
@@ -12,8 +12,20 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {
-  // add your implementation here
-}
+const average = (array) => {
+  let result = 0;
+  let media = 0;
+  if (array.length === 0) {
+    return undefined;
+  }
+  for (let i = 0; i < array.length; i += 1) {
+    if (typeof array[i] !== 'number' || array.length === 0) {
+      return undefined;
+    } 
+    result += array[i];
+    media = result / array.length;
+  }
+  return Math.round(media);
+};
 
-module.exports = average
+module.exports = average;
